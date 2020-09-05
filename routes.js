831340a0b45b49656204cdbfa7a3a021572668a7
Thais <thais.rocha@dextra-sw.com>
2020-09-05 13:43:11 -0300

    Configurando rotas

---
+++ routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { creatAppContainer, createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Splash from "./container/Splash";
+import Principal from "./container/Principal";
+import Login from "./container/Login";
+import Cadastrar from "./container/Cadastrar";
+import CadastrarMulher from "./container/CadastrarMulher";
+import ScreenTipDidYouKnow from "./container/ScreenTipDidYouKnow";
+import ScreenTipAboutWoU from "./container/ScreenTipAboutWoU";
+import Home from './container/Home';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Splash: Splash,
+    Principal: Principal,
+    Entrar: Login,
+    Cadastrar: Cadastrar,
+    CadastrarMulher: CadastrarMulher,
+    DidYouKnow: ScreenTipDidYouKnow,
+    AboutWoU: ScreenTipAboutWoU,
+    Home: Home,
+  })
+);
+
+export default Routes;
