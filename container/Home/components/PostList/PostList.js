2f7a1dafff5b3b697414f20a92b4269bf0ae1b51
thaisOliveiraRocha <thais.oliveirarocha@hotmail.com>
2020-09-05 22:24:08 -0300

    Componente de listagem de publicações

---
+++ container/Home/components/PostList/PostList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Button, Text, ImageBackground, StyleSheet } from "react-native";
+import CardPost from "../CardPost";
+
+export const PostList = ({ navigation, postList }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{postList.title}</Text>
+      {postList.posts.map((post) => (
+        <View style={styles.postContainer}>
+          <CardPost cardPost={post} />
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    borderTopWidth: 2,
+    borderColor: "#CCCCCC",
+    marginHorizontal: 16,
+  },
+  postContainer: {
+    marginVertical: 8,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#333333",
+    marginTop: 8,
+  },
+});
+
+export default PostList;
