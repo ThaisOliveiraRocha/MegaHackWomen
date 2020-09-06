bd78307431a6f281e204b73f1becac218a872f99
thaisOliveiraRocha <thais.oliveirarocha@hotmail.com>
2020-09-05 22:23:29 -0300

    Componente de listagem de sugestão de amizade

---
+++ container/Home/components/Friendship/Friendship.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import CardFriend from "../CardFriend";
+
+export const Friendship = ({ navigation, content }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContent}>
+        <Text style={styles.title}>{content.title}</Text>
+      </View>
+      <ScrollView horizontal>
+        {content.friendship.map((friend) => (
+          <View style={styles.card}>
+            <CardFriend navigation={navigation} friend={friend} />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    marginHorizontal: 16,
+    borderRadius: 10,
+    height: 320,
+  },
+  card: {
+    width: 140,
+    height: 260,
+    marginHorizontal: 8,
+    marginVertical: 8,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#000",
+    marginLeft: 8,
+  },
+  titleContent: {
+    marginVertical: 8,
+  },
+});
+
+export default Friendship;
---
+++ container/Home/components/Friendship/index.js
@@ -0,0 +1,3 @@
+import Friendship from './Friendship';
+
+export default Friendship;
