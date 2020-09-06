f634447071299b1cd3646d56fb8a2284647dd093
thaisOliveiraRocha <thais.oliveirarocha@hotmail.com>
2020-09-05 22:22:05 -0300

    Mock do conteúdo da home

---
+++ container/Home/mock/content.js
@@ -0,0 +1,77 @@
+const content = {
+  friendshipList: {
+    title: "Sugestões de amizade",
+    friendship: [
+      {
+        img: "",
+        name: "Amanda",
+        areaAtuacao: "UI/UX",
+        button: "Conectar",
+      },
+      {
+        img: "",
+        name: "Carla",
+        areaAtuacao: "Historiadora",
+        button: "Conectar",
+      },
+      {
+        img: "",
+        name: "Jaqueline",
+        areaAtuacao: "Desenvolvedora",
+        button: "Conectar",
+      },
+      {
+        img: "",
+        name: "Tayná",
+        areaAtuacao: "Coach",
+        button: "Conectar",
+        status: "send",
+      },
+      {
+        img: "",
+        name: "Julia",
+        areaAtuacao: "UI/UX",
+        button: "Conectar",
+        status: "send",
+      },
+    ],
+  },
+  postList: {
+    title: "Mar Aberto",
+    posts: [
+      {
+        img: "",
+        name: "Beatriz",
+        date: "1h",
+        textBody:
+          "O apagamento das mulheres da área de ciência, tecnologia e engenharia, é histórico e socialmente construído",
+        likes: "120 curtidas",
+        comments: "4 comentários",
+        buttonLike: "Curtir",
+        buttonComment: "Comentar",
+      },
+      {
+        img: "",
+        name: "Carol",
+        date: "5h",
+        textBody: "comi batata ontem",
+        likes: "2109 curtidas",
+        comments: "530 comentários",
+        buttonLike: "Curtir",
+        buttonComment: "Comentar",
+      },
+      {
+        img: "",
+        name: "Gabrieli",
+        date: "1d",
+        textBody: "comi batata ontem",
+        likes: "1 curtida",
+        comments: "300 comentários",
+        buttonLike: "Curtir",
+        buttonComment: "Comentar",
+      },
+    ],
+  },
+};
+
+export default content;
