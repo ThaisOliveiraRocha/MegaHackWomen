735f9ff1ff68efc0aab95cecdcc396d11704f94e
thaisOliveiraRocha <thais.oliveirarocha@hotmail.com>
2020-09-05 22:20:57 -0300

    Página Home

--- container/Home/Home.js
+++ container/Home/Home.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { View, Button, Text, ImageBackground, StyleSheet } from "react-native";
+import {
+  View,
+  Button,
+  ScrollView,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
+import Friendship from "./components/Friendship";
+import PostList from "./components/PostList";
+import WritePost from "./components/WritePost";
+import content from "./mock/content";
 
 export const Home = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.writePost}>
+          <WritePost navigation={navigation} />
+        </View>
+        <View style={styles.friendship}>
+          <Friendship
+            navigation={navigation}
+            content={content.friendshipList}
+          />
+        </View>
+        <View>
+          <PostList postList={content.postList} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,6 +36,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
+    backgroundColor: "#fff",
+  },
+  friendship: {
+    marginBottom: 8,
+  },
+  writePost: {
+    borderWidth: 2,
+    borderColor: "#CCCCCC",
+    borderRadius: 10,
+    height: 56,
+    marginHorizontal: 16,
+    marginTop: 56,
   },
 });
 
