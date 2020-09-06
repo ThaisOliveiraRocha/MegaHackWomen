bc8b4eaea587f229b0001c17f825b1b20c58625d
thaisOliveiraRocha <thais.oliveirarocha@hotmail.com>
2020-09-05 22:24:51 -0300

    Componente área de escrita nova publicação

---
+++ container/Home/components/WritePost/WritePost.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export const WritePost = ({ navigation }) => {
+  return (
+    <TouchableOpacity>
+      <View style={styles.container}>
+        <Image source={require("../../../../assets/images/note.png")} />
+        <Text style={styles.title}>No que você está pensando?</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    marginHorizontal: 8,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    textAlign: "left",
+    paddingVertical: 12,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#333333",
+    marginLeft: 8,
+  },
+});
+
+export default WritePost;
